7aaa3fb1706e83b47bcb29519a11718a8a8d5dab
JORGEABARROCO <153916367+JORGEABARROCO@users.noreply.github.com>
2024-04-28 18:48:10 -0300

    Update EmailInUseException.java

--- src/main/java/br/com/ada/locatecar/exceptions/EmailInUseException.java
+++ src/main/java/br/com/ada/locatecar/exceptions/EmailInUseException.java
@@ -1,7 +1,9 @@
 package br.com.ada.locatecar.exceptions;
 
 public class EmailInUseException extends RuntimeException {
-    public EmailInUseException(String message) {
-        super(message);
+
+    public EmailInUseException() {
+        super("Uh oh! The email you entered is already associated with an account. " +
+                "Please try a different email address or consider logging in if this is your account.");
     }
 }
