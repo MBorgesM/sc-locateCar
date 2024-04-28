c64ce53f77c2eb75ef091cdf388e81810f6207d1
JORGEABARROCO <153916367+JORGEABARROCO@users.noreply.github.com>
2024-04-28 18:51:04 -0300

    Update MessageResponse.java

--- src/main/java/br/com/ada/locatecar/payload/response/MessageResponse.java
+++ src/main/java/br/com/ada/locatecar/payload/response/MessageResponse.java
@@ -1,12 +1,15 @@
-package br.com.ada.locatecar.payload.response;
+// ... (previous code)
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+    @ExceptionHandler(EmailInUseException.class)
+    public ResponseEntity<MessageResponse> handleEmailInUseException(EmailInUseException ex) {
+        // Use a more informative status code
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new MessageResponse("That email address is already in use. Please try a different one."));
+    }
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class MessageResponse {
-    private String message;
-}
+    @ExceptionHandler(DocumentInUseException.class)
+    public ResponseEntity<MessageResponse> handleDocumentInUseException(DocumentInUseException ex) {
+        // Use a more informative status code
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new MessageResponse(ex.getMessage()));
+    }
