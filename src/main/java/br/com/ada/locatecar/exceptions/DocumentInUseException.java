aaad32a1e146dc8bfb7aca2f054c59e600af9c83
JORGEABARROCO <153916367+JORGEABARROCO@users.noreply.github.com>
2024-04-28 18:46:04 -0300

    Update DocumentInUseException.java

--- src/main/java/br/com/ada/locatecar/exceptions/DocumentInUseException.java
+++ src/main/java/br/com/ada/locatecar/exceptions/DocumentInUseException.java
@@ -1,7 +1,9 @@
 package br.com.ada.locatecar.exceptions;
 
 public class DocumentInUseException extends RuntimeException {
-    public DocumentInUseException(String message) {
-        super(message);
+
+    public DocumentInUseException(String documentType) {
+        super("Hold on! It seems the " + documentType + " you provided is already in use. " +
+                "Please double-check or contact support if you believe this is an error.");
     }
 }
