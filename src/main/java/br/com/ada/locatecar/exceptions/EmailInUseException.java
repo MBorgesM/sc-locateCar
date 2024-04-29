af65aac0a4e147dd25b47260a0ca30839f184146
DanielMartinsSouza <danielmartsouza@gmail.com>
2024-04-29 15:39:05 -0300

    FIX: Exceptions message

--- src/main/java/br/com/ada/locatecar/exceptions/EmailInUseException.java
+++ src/main/java/br/com/ada/locatecar/exceptions/EmailInUseException.java
@@ -1,9 +1,7 @@
 package br.com.ada.locatecar.exceptions;
 
 public class EmailInUseException extends RuntimeException {
-
-    public EmailInUseException() {
-        super("Uh oh! The email you entered is already associated with an account. " +
-                "Please try a different email address or consider logging in if this is your account.");
+    public EmailInUseException(String message) {
+        super(message);
     }
 }
--- src/main/java/br/com/ada/locatecar/payload/response/MessageResponse.java
+++ src/main/java/br/com/ada/locatecar/payload/response/MessageResponse.java
@@ -1,15 +1,12 @@
-// ... (previous code)
+package br.com.ada.locatecar.payload.response;
 
-    @ExceptionHandler(EmailInUseException.class)
-    public ResponseEntity<MessageResponse> handleEmailInUseException(EmailInUseException ex) {
-        // Use a more informative status code
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new MessageResponse("That email address is already in use. Please try a different one."));
-    }
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-    @ExceptionHandler(DocumentInUseException.class)
-    public ResponseEntity<MessageResponse> handleDocumentInUseException(DocumentInUseException ex) {
-        // Use a more informative status code
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new MessageResponse(ex.getMessage()));
-    }
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
