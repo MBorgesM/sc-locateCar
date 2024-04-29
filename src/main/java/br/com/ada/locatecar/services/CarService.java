a60ddbed5154192de796a233070961cfb73b6018
DanielMartinsSouza <danielmartsouza@gmail.com>
2024-04-29 15:25:07 -0300

    FEAT: init CarService and CarController

---
+++ src/main/java/br/com/ada/locatecar/services/CarService.java
@@ -0,0 +1,48 @@
+package br.com.ada.locatecar.services;
+
+import br.com.ada.locatecar.dtos.CarDto;
+import br.com.ada.locatecar.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class CarService {
+    private static final String URL = "https://freetestapi.com";
+    private static final String URL_ALL_CARS = "/api/v1/cars ";
+    private static final String URL_CAR = "/api/v1/cars/{idCar} ";
+
+    @Autowired
+    private CarRepository carRepository;
+
+    public List<CarDto> getAllCar(){
+        CarDto[] cars = WebClient.create(URL)
+                .get()
+                .uri(URL_ALL_CARS)
+                .retrieve()
+                .bodyToMono(CarDto[].class)
+                .block();
+
+        return List.of(Objects.requireNonNull(cars));
+    }
+
+    public List<CarDto> sugarDistritos() {
+        List<CarDto> cars = this.getAllCar();
+        return this.saveAll(cars);
+    }
+
+    private List<CarDto> saveAll(List<CarDto> cars) {
+        return cars.stream()
+                .map(this::save)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private CarDto save(CarDto carDto) {
+        return this.carRepository.save(carDto);
+    }
+}
